26b9c312a0e7ad21fb73cba16f651f661cee463d
fmaachadoo <filipe__machado@hotmail.com>
2023-04-10 16:44:47 -0300

    feat: add blob sketch

--- GithubRecomendations/src/App.jsx
+++ GithubRecomendations/src/App.jsx
@@ -4,11 +4,22 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './components/organisms/Login'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
+  const blob = document.querySelector('.blob')
+
+  document.body.onpointermove = event => {
+    const { clientX, clientY } = event;
+
+    blob.style.left `${clientX}px`;
+    blob.style.top `${clientY}px`;
+  }
+
   return (
     <div className="App">
+      {/*<span className="blob" />*/}
       <Login/>
     </div>
   )
