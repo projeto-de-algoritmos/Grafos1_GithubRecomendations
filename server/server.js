99b64cc037bae680f8272ce9d704a3ed1d63420f
fmaachadoo <filipe__machado@hotmail.com>
2023-04-30 09:27:19 -0300

    fix: server port is now 800

--- server/server.js
+++ server/server.js
@@ -65,4 +65,4 @@ app.get('/getUserData', async function (req, res) {
 }); 
 
 
-app.listen(5000, () => console.log('Server started on port 5000'));
+app.listen(8000, () => console.log('Server started on port 8000'));
