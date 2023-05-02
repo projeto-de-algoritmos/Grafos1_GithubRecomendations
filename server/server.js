af18461d8d5e75ad4c1981bb74add2dbdfab887a
fmaachadoo <filipe__machado@hotmail.com>
2023-05-01 21:35:02 -0300

    refactor: fix line identation

--- server/server.js
+++ server/server.js
@@ -10,8 +10,8 @@ var app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.options('*',cors());
-app.use(function(req, res, next) {
+app.options('*', cors());
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -21,9 +21,7 @@ app.use(function(req, res, next) {
 
 async function getUserData(req) {
     const response = await fetch('https://api.github.com/user', {
-        mode: 'cors',
-        method: 'GET',
-        headers: {
+        mode: 'cors', method: 'GET', headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
             'Authorization': req.get("Authorization"),
@@ -34,47 +32,50 @@ async function getUserData(req) {
 }
 
 function customScoring(yourFriendsSet, friend, friendOfFriendLogin) {
-  const yourCommonFriends = new Set([...yourFriendsSet].filter(x => friend.adjacent.includes(x)));
+    const yourCommonFriends = new Set([...yourFriendsSet].filter(x => friend.adjacent.includes(x)));
 
-  // Since we don't have the friends of friends adjacency list, we cannot calculate the common friends with your friend
-  // We can only calculate the points based on the common friends between you and the friend of a friend
-  const yourCommonFriendsPoints = yourCommonFriends.size * 2;
+    // Since we don't have the friends of friends adjacency list, we cannot calculate the common friends with your friend
+    // We can only calculate the points based on the common friends between you and the friend of a friend
+    const yourCommonFriendsPoints = yourCommonFriends.size * 2;
 
-  return yourCommonFriendsPoints;
+    return yourCommonFriendsPoints;
 }
 
 function recommendFriends(adjacencyList) {
-  const yourFriendsSet = new Set(adjacencyList[0].adjacent);
-  const recommendations = new Map();
+    const yourFriendsSet = new Set(adjacencyList[0].adjacent);
+    const recommendations = new Map();
 
-  adjacencyList.slice(1).forEach(friend => {
-    friend.adjacent.forEach(friendOfFriendLogin => {
-      if (!yourFriendsSet.has(friendOfFriendLogin) && adjacencyList[0].login !== friendOfFriendLogin) {
-        const score = customScoring(yourFriendsSet, friend, friendOfFriendLogin);
+    adjacencyList.slice(1).forEach(friend => {
+        friend.adjacent.forEach(friendOfFriendLogin => {
+            if (!yourFriendsSet.has(friendOfFriendLogin) && adjacencyList[0].login !== friendOfFriendLogin) {
+                const score = customScoring(yourFriendsSet, friend, friendOfFriendLogin);
 
-        if (recommendations.has(friendOfFriendLogin)) {
-          recommendations.set(friendOfFriendLogin, {
-            score: Math.max(score, recommendations.get(friendOfFriendLogin).score),
-            connections: recommendations.get(friendOfFriendLogin).connections + 1
-          });
-        } else {
-          recommendations.set(friendOfFriendLogin, {score, connections: 1});
-        }
-      }
+                if (recommendations.has(friendOfFriendLogin)) {
+                    recommendations.set(friendOfFriendLogin, {
+                        score: Math.max(score, recommendations.get(friendOfFriendLogin).score),
+                        connections: recommendations.get(friendOfFriendLogin).connections + 1
+                    });
+                } else {
+                    recommendations.set(friendOfFriendLogin, {
+                        score, connections: 1
+                    });
+                }
+            }
+        });
     });
-  });
 
-  const sortedRecommendations = [...recommendations.entries()].sort((a, b) => {
-    if (b[1].score === a[1].score) {
-      return b[1].connections - a[1].connections;
-    } else {
-      return b[1].score - a[1].score;
-    }
-  });
+    const sortedRecommendations = [...recommendations.entries()].sort((a, b) => {
+        if (b[1].score === a[1].score) {
+            return b[1].connections - a[1].connections;
+        } else {
+            return b[1].score - a[1].score;
+        }
+    });
 
-  return sortedRecommendations.map(([friendLogin, {score, connections}]) => ({login: friendLogin, score, connections}));
+    return sortedRecommendations.map(([friendLogin, {
+        score, connections
+    }]) => ({login: friendLogin, score, connections}));
 }
-
 
 
 app.get('/getAccessToken', async function (req, res) {
@@ -82,28 +83,25 @@ app.get('/getAccessToken', async function (req, res) {
     const rootURl = 'https://github.com/login/oauth/access_token';
 
     const options = {
-        code: code,
-        client_id: CLIENT_ID,
-        client_secret: CLIENT_SECRET,
-      };
+        code: code, client_id: CLIENT_ID, client_secret: CLIENT_SECRET,
+    };
 
     const headers = {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
+        'Content-Type': 'application/json', 'Accept': 'application/json',
     };
 
     const qs = new URLSearchParams(options);
-  
+
     const fullURL = `${rootURl}?${qs.toString()}`;
     console.log(fullURL)
 
     await fetch(fullURL, {
-        mode: 'cors',
-        method: 'POST',
-        headers: headers,
+        mode: 'cors', method: 'POST', headers: headers,
     })
-    .then(response => {return response.json()})
-    .then(data => res.json(data));
+        .then(response => {
+            return response.json()
+        })
+        .then(data => res.json(data));
 });
 
 app.get('/getFriendsofFriendsGraph', async function (req, res) {
@@ -114,35 +112,34 @@ app.get('/getFriendsofFriendsGraph', async function (req, res) {
     const items = [];
 
     const friends = [];
-    req.get("Authorization"); 
+    req.get("Authorization");
     await fetch('https://api.github.com/user/following', {
-        mode: 'cors',
-        method: 'GET',
-        headers: {
+        mode: 'cors', method: 'GET', headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/vnd.github+json',
             'Authorization': req.get("Authorization"),
             'X-GitHub-Api-Version': '2022-11-28',
-        }})
-    .then(response => {
-        if(response.ok){
-            return response.json();
         }
-        throw new Error('Network response was not ok.'); // if response is not ok, throws error
-    }) 
-    .then(data => {
-        if (Array.isArray(data)) {
-          data.map(item => item).forEach(friend => {
-            friends.push(data.map(item => item))
-          });
-        } else {
-          console.log('data is not an array or is undefined');
-        }
-      })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-    
+    })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error('Network response was not ok.'); // if response is not ok, throws error
+        })
+        .then(data => {
+            if (Array.isArray(data)) {
+                data.map(item => item).forEach(friend => {
+                    friends.push(data.map(item => item))
+                });
+            } else {
+                console.log('data is not an array or is undefined');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+
 
     user.adjacent = friends[0].map(item => item.login); //adds friends to user adj list
     friends[0].forEach(item => item.adjacent = [user.login]); //adds user to friends adj list
@@ -150,24 +147,25 @@ app.get('/getFriendsofFriendsGraph', async function (req, res) {
 
     for (let i = 0; i < friends[0].length; i++) {
         await fetch('https://api.github.com/users/' + friends[0][i].login + '/followers', {
-        mode: 'cors',
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/vnd.github+json',
-            'Authorization': req.get("Authorization"),
-            'X-GitHub-Api-Version': '2022-11-28',
-        }})
-        .then(response => {return response.json()})
-        .then(data => {
-            if (Array.isArray(data)) {
-              data.map(item => item).forEach(friend => {
-                friends[0][i].adjacent.push(friend.login);
-              });
-            } else {
-              console.log('data is not an array or is undefined');
+            mode: 'cors', method: 'GET', headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/vnd.github+json',
+                'Authorization': req.get("Authorization"),
+                'X-GitHub-Api-Version': '2022-11-28',
             }
-          });
+        })
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                if (Array.isArray(data)) {
+                    data.map(item => item).forEach(friend => {
+                        friends[0][i].adjacent.push(friend.login);
+                    });
+                } else {
+                    console.log('data is not an array or is undefined');
+                }
+            });
     }
     // console.log(items)
     // items.shift();
@@ -176,7 +174,6 @@ app.get('/getFriendsofFriendsGraph', async function (req, res) {
     console.log(friendRecommendations)
     res.send(friendRecommendations);
 });
-
 
 
 app.listen(8000, () => console.log('Server started on port 8000'));
