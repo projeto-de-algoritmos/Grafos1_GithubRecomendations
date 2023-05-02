fd8f1a3250f397a6dcfe9047160270a6af73dad2
fmaachadoo <filipe__machado@hotmail.com>
2023-05-01 22:43:43 -0300

    fix: fixes css
    Co-authored-by: freitasc <caina2000@gmail.com>

--- GithubRecomendations/src/components/organisms/FrindsOfFriends.jsx
+++ GithubRecomendations/src/components/organisms/FrindsOfFriends.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import '../../App.css'
 import OutlineCard from '../atoms/OutlineCard'
 import Text from '../atoms/Text'
@@ -15,27 +15,37 @@ function App() {
         window.location.replace('/');
     }
 
-
     return (
-        <OutlineCard
-            background-color='white'
-            height='70dvh'
-            width='fit-content'
-            padding='clamp(1em, 2vw ,2em)'
-        >
-            <GlitchedText className="glitchedText">FOLLOW RECOMENDATIONS</GlitchedText>
-            <Graph />
-            <Text
-                size='1.5em'
-                margin='0 2em 1em 2em'
-            >
-                Clique para deslogar
-            </Text>
-            <a className="logoff-button" onClick={() => handleClick()}>
-                <Text size='2em;'>Logoff</Text>
-                <img src={githubIcon} className="logo github" alt="Github logo" />
-            </a>
-        </OutlineCard>
+        <>
+            <h1 className="glitchedText">FOLLOW
+                RECOMENDATIONS
+            </h1>
+            <div>
+                <OutlineCard
+                    background-color='white'
+                    height='fit-content'
+                    width='fit-content'
+                    padding='clamp(1em, 2vw ,2em)'
+                >
+                    <Graph/>
+
+                </OutlineCard>
+
+                <Text
+                    size='1.5em'
+                    margin='0 2em 1em 2em'
+                >
+                    Clique para deslogar
+                </Text>
+                <a className="logoff-button" onClick={() => handleClick()}>
+                    <Text size='2em;'>Logoff</Text>
+                    <img src={githubIcon} className="logo github"
+                         alt="Github logo"/>
+                </a>
+            </div>
+
+        </>
+
     )
 }
 
